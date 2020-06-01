ad9702b2d968113400392e9109f356abc80ee431
Mikita <nikita1597538426@gmail.com>
2020-06-01 17:25:06 +0300

    some adds

--- start.py
+++ start.py
@@ -1,9 +1,31 @@
+from collections import namedtuple # тк для него работает методы через . 
+
 from flask import Flask
+from flask import render_template
+from flask import redirect
+from flask import url_for
+from flask import request
 
 
 app = Flask(__name__)
 
+Message = namedtuple('Message', 'text tag')
+messages = []
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+
+@app.route('/main', methods=['GET'])
+def main():
+    return render_template('main.html', messages=messages)
+
+
+@app.route('/add_message', methods=['POST'])
+def add_message():
+    text = request.form['text']
+    tag = request.form['tag']
+    messages.append(Message(text, tag))
+    return redirect(url_for('main'))
