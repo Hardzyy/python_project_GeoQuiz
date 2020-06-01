d3880bc1af400f751f4d59793c8312540adbbda7
Mikita <nikita1597538426@gmail.com>
2020-06-01 17:53:15 +0300

    changes

--- start.py
+++ start.py
@@ -1,16 +1,26 @@
-from collections import namedtuple # тк для него работает методы через . 
+from collections import namedtuple # тк для него работает методы через .
 
 from flask import Flask
 from flask import render_template
 from flask import redirect
 from flask import url_for
 from flask import request
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:123@localhost/py_sweater'
+db = SQLAlchemy(app)
 
-Message = namedtuple('Message', 'text tag')
-messages = []
+
+class Massage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(1024), nullable=False)
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(32), nullable=False)
 
 
 @app.route('/', methods=['GET'])
